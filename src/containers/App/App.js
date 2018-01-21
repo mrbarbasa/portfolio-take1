4b35a6db4f170b0647c2759ba5fca7a1f4600962
Marifel Barbasa <mrbarbasa@gmail.com>
2018-01-20 22:18:48 -1000

    Add minimal content to What's New?, About, Projects, and Skills sections

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import styles from './App.module.css';
-import { Header, Menu, SquareGrid } from '../../components';
+import {
+  Header,
+  Heading,
+  Menu,
+  Section,
+  Square,
+  SquareGrid,
+} from '../../components';
 
 class App extends Component {
   render() {
@@ -8,16 +15,53 @@ class App extends Component {
       <div className={styles.container}>
         <Header title="Marifel Barbasa" />
         <Menu />
-        <SquareGrid>
-          <div />
-          <div />
-          <div />
-          <div />
-          <div />
-          <div />
-          <div />
-          <div />
-        </SquareGrid>
+        <Section>
+          <Heading title="What's New?" />
+          <SquareGrid>
+            <Square text="Reading Mastering Bitcoin" onHoverText="Hello" />
+            <Square text="Hacking on this site" onHoverText="Hello" />
+            <Square text="Buying cryptocurrencies" onHoverText="Hello" />
+            <Square
+              text="Researching machine learning programs"
+              onHoverText="Hello"
+            />
+          </SquareGrid>
+        </Section>
+        <Section backgroundColor="#2B303B">
+          <Heading title="About" color="#C0C5CE" />
+          <p className={styles.text}>
+            I'm a full-stack web developer passionate about web technologies,
+            open-source software, and learning, striving to effect positive
+            changes in tech and its community.
+          </p>
+        </Section>
+        <Section backgroundColor="#F0F0F0">
+          <Heading title="Projects" />
+          <SquareGrid>
+            <Square text="js-zombies" onHoverText="Hello" />
+            <Square text="Werewolf" onHoverText="Hello" />
+            <Square text="W.A.T.E.R.S." onHoverText="Hello" />
+            <Square text="Lazy Chef Bot" onHoverText="Hello" />
+          </SquareGrid>
+        </Section>
+        <Section backgroundColor="#2B303B">
+          <Heading title="Skills" color="#C0C5CE" />
+          <ul className={styles.text}>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>PostgreSQL</li>
+          </ul>
+        </Section>
+        <Section backgroundColor="#F0F0F0">
+          <Heading title="Experience" />
+        </Section>
+        <Section backgroundColor="#2B303B">
+          <Heading title="Education" color="#C0C5CE" />
+        </Section>
+        <Section backgroundColor="#F0F0F0">
+          <Heading title="Links" />
+        </Section>
       </div>
     );
   }
